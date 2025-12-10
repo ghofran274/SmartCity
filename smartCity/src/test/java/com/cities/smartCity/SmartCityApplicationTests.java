7ec5708130c684077b521f2a567cef0a24c6ad17
ghofran274 <gaithkarkeni60@gmail.com>
2025-12-10 21:52:04 +0100

    City Commit

---
+++ smartCity/src/test/java/com/cities/smartCity/SmartCityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cities.smartCity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
